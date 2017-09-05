1e723fcf6975ff7f28777750c36988be9661e4bf
Xie Yongshan <yongshanhaiyan@163.com>
2017-09-05 14:32:25 +0800

    update CacheDemo 1111

--- src/cn/itcast/heima2/CacheDemo.java
+++ src/cn/itcast/heima2/CacheDemo.java
@@ -10,6 +10,7 @@ public class CacheDemo {
 	private Map<String, Object> cache = new HashMap<String, Object>();
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		System.out.println(111111);
 
 	}
 
